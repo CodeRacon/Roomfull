da00148ee86efa9c28861792c39e34aa4ff37bb1
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-01 16:31:43 +0100

    feat: extended Room class with set changable properties

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
+  use HasFactory;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'type',
+    'capacity',
+    'min_duration',
+    'price_per_hour',
+    'is_active',
+  ];
+
+  /**
+   * Get the bookings for the room.
+   */
   public function bookings()
   {
     return $this->hasMany(Booking::class);
