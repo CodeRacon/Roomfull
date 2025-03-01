2071b3d28a41fd5f8631890eaf375132e9c80b31
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-01 19:10:45 +0100

    feat(routing): added the RoomController to the routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomController;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Route::resource('rooms', RoomController::class);
