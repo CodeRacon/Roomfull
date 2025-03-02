40c28270534ce6a45be283c91b9aa2cda00f061c
CodeRacon <michael.buschmann.86@gmail.com>
2025-03-02 16:47:47 +0100

    Implement role-based user system with admin and customer roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,78 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Booking;
+
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+  /** @use HasFactory<\Database\Factories\UserFactory> */
+  use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+  // define role-constants
+  public const ROLE_ADMIN = 'admin';
+  public const ROLE_CUSTOMER = 'customer';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var list<string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'role',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var list<string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array
+  {
+    return [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
     ];
+  }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  public function bookings()
+  {
+    return $this->hasMany(Booking::class);
+  }
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+  /**
+   * proofs, if user is admin
+   *
+   * @return bool
+   */
+  public function isAdmin(): bool
+  {
+    return $this->role === self::ROLE_ADMIN;
+  }
+
+  /**
+   * proofs, if user is customer
+   *
+   * @return bool
+   */
+  public function isCustomer(): bool
+  {
+    return $this->role === self::ROLE_CUSTOMER;
+  }
 }
